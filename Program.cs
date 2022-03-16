1710dc437aac6c3b658e4cea7dafc9b7490bc7cd
Tresor Tshimanga <tshims79@gmail.com>
2022-03-16 13:45:41 +0200

    Other commonly used extension methods are OrderBy and ThenBy, used for sorting a sequence

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace LinqWithObjects
            // var query = names.Where(new Func<string, bool>(NameLongerThanFour));
            //var query = names.Where(NameLongerThanFour);
            var query = names.Where(name => name.Length > 4)
-                            .OrderBy(name.Length);
+                            .OrderBy(name => name.Length);
             foreach (string item in query)
             {
                 WriteLine(item);
