64a9399e299a6894ca61ad0f0d3164740d4de3f2
Tresor Tshimanga <tshims79@gmail.com>
2022-03-16 13:14:03 +0200

    Creating the  Filtering entities with Where

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqWithObjects
+{
+    class Program
+    {
+
+        static void LinqWithArrayOfStrings()
+        {
+            var names = new string[] 
+            {
+                "Michael",
+                "Pam",
+                "Jim",
+                "Dwight",
+                "Angela",
+                "Ida",
+                "Shulamite",
+                "Grace",
+                "Megane"
+            };
+            var query = names.Where
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
